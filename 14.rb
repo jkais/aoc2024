9a58b5c74a9ca52d7decb90efb1afabb8d4e3b86
Jan Kaiser <kaiser@hey.com>
2024-12-15 22:22:12 +0100

    day 14

--- 14.rb
+++ 14.rb
@@ -1,4 +1,6 @@
 require_relative("matrix")
+require 'chunky_png'
+require 'mini_magick'
 
 def robots(filename)
   if filename == "14/test.txt"
@@ -55,6 +57,52 @@ def robots(filename)
   score = first * second * third * fourth
 
   return score
+end
+
+def xmas(filename)
+  if filename == "14/test.txt"
+    x = 11
+    y = 7
+    return "Not needed"
+  else
+    x = 101
+    y = 103
+  end
+
+  matrix = Array.new(y) { Array.new(x, 0) }
+
+  map = Matrix.new([], file: false)
+  map.set_matrix(matrix)
+
+  bots = File.read(filename).split("\n").map { |bot| Bot.new(bot, x, y) }
+
+  30000.times do |i|
+    puts i
+    matrix = Array.new(y) { Array.new(x, 0) }
+    map.set_matrix(matrix)
+    bots.map &:move
+
+    bots.each do |bot|
+      map.set(bot.px, bot.py, (map.at(bot.px, bot.py) + 1))
+    end
+
+    if i > 0
+      image = ChunkyPNG::Image.new(x+1, y+1, ChunkyPNG::Color::WHITE)
+
+      for cy in 0.upto(y - 1) do
+        for cx in 0.upto(x - 1) do
+          image[cx,cy] = ChunkyPNG::Color.rgb(256, 0, 0) if map.at(cx, cy) != 0
+        end
+      end
+
+      name = "xmas/#{i}.png"
+      image.save(name)
+      image = MiniMagick::Image.open(name)
+      image.resize "#{x*10}x#{y*10}"
+      image.write(name)
+
+    end
+  end
 end
 
 class Bot
