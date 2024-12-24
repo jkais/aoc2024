06bf3d9ef134f49b1f95662333646d90eafc6ca7
Jan Kaiser <kaiser@hey.com>
2024-12-24 15:55:47 +0100

    update on matrix and time

--- matrix.rb
+++ matrix.rb
@@ -78,13 +78,3 @@ class Matrix
 
   private
 end
-
-def time
-  puts "Took " + (Time.now - @time).to_s + "s"
-end
-
-def reset
-  @time = Time.now
-end
-
-reset
--- time.rb
+++ time.rb
@@ -1,9 +1,24 @@
-def time
-  puts "Took " + (Time.now - @time).to_s + "s"
+def time(mess="")
+  t = seconds
+  hours = t.to_i / 3600
+  minutes = t.to_i / 60
+  seconds = (t % 60).round(3)
+
+  to_words = "#{seconds}s"
+  to_words = "#{minutes}m #{to_words}" if minutes > 0
+  to_words = "#{hours}h #{to_words}" if hours > 0
+
+  first = mess == "" ? "Took" : "#{mess} took"
+
+  puts "#{first} #{to_words}"
 end
 
 def reset
   @time = Time.now
+end
+
+def seconds
+  Time.now - @time
 end
 
 reset
